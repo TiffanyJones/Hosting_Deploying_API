fc0b9e4564080a692424f886a91b4a962f5713bc
TiffanyJones <134651957+TiffanyJones@users.noreply.github.com>
2024-05-10 13:31:39 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const main = document.querySelector("main");
+const roverList = document.querySelector(".rover-list");
+const errorMsg = document.querySelector(".error-msg");
+const dateInput = document.querySelector("#date-input");
+const submitButton = document.querySelector("#submit-button");
+
+const apiKey = "ShEB5biaOhzZvSSF3A9Xc5BZY22BbOhh1FQeB3YQ";
+
+
+async function fetchData(url, handleData) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    handleData(data);
+  } catch (error) {
+    errorMsg.style.display = "block";
+    errorMsg.innerHTML = "There is a problem with your fetch: " + error;
+  }
+}
+
+
+
+function handleRoverData(data) {
+  roverList.innerHTML = "";
+  if (!data.photos.length) {
+    roverList.innerHTML =
+      "<li>No photos were taken on this date. Please select another date.</li>";
+  } else {
+    const photos = data.photos
+      .map(photo => `<li><img src="${photo.img_src}" alt="Rover photo"></li>`)
+      .join("");
+    roverList.insertAdjacentHTML("beforeend", photos);
+  }
+}
+
+submitButton.addEventListener("click", function () {
+  roverList.innerHTML = "Loading...";
+  const date = dateInput.value
+    ? dateInput.value
+    : new Date().toISOString().slice(0, 10);
+
+  const roverURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${apiKey}`;
+
+  fetchData(roverURL, handleRoverData);
+});
